720381d6c6aa89b00ad226d334ed1fe6beb70a09
Akash-Narwade01 <akashnarwade969@gmail.com>
2025-08-29 00:50:07 +0530

    Add files via upload

---
+++ assi2.2.py
@@ -0,0 +1,9 @@
+# Initialize a variable to store the sum
+total_sum = 0
+
+# Loop through numbers from 1 to 50 and add them to the sum
+for i in range(1, 51):
+    total_sum += i
+
+# Print the final sum
+print(f"The sum of integers from 1 to 50 is: {total_sum}")
